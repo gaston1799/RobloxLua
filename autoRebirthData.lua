fcf92e393ea8caa54f7fca54886766f4e42e28c2
Gaston <35858201+gaston1799@users.noreply.github.com>
2025-11-29 20:06:51 -0500

    Add autoRebirthData with catalyst information
    This file contains a table of catalysts, their names, and associated items for a game.

---
+++ autoRebirthData.lua
@@ -0,0 +1,230 @@
+return {
+                ["Catalyst of Thunder"] = {
+                    name = "Draedon's Gauntlet",
+                    items = {
+                        "True Book of Knowledge",
+                        "Tempest Refiner",
+                        "Lightningbolt Predictor",
+                        "Azure Purifier",
+                        "Mystical Thunder",
+                        "Tesla Refuter"
+                    },
+                    Catalyst = "Catalyst of Thunder"
+                },
+                ["Catalyst of Void"] = {
+                    name = "Daestrophe",
+                    items = {
+                        "True Book of Knowledge",
+                        "The Fissure",
+                        "The Daegelart",
+                        "Overlord's Telamonster",
+                        "Eternal Fracture",
+                        "Void Drive"
+                    },
+                    Catalyst = "Catalyst of Void"
+                },
+                ["Catalyst of Spirits"] = {
+                    name = "Delta Phantom",
+                    items = {
+                        "True Book of Knowledge",
+                        "Eternal Limbo",
+                        "Brimstone Spires",
+                        "Dark Illuminator",
+                        "Forbidden Magic",
+                        "Anguished Guardian of the Gate"
+                    },
+                    Catalyst = "Catalyst of Spirits"
+                },
+                ["Catalyst of Destruction"] = {
+                    name = "Pandora's Box",
+                    items = {
+                        "True Book of Knowledge",
+                        "Dreamer's Blight",
+                        "Dreamer's Valor",
+                        "Dreamer's Nightmare",
+                        "Dreamer's Terror",
+                        "Devourer of Nightmares"
+                    },
+                    Catalyst = "Catalyst of Destruction"
+                },
+                ["Catalyst of the Supreme"] = {
+                    name = "Elysium Solemnity",
+                    items = {
+                        "True Book of Knowledge",
+                        "True Overlord Device",
+                        "Aethereal Synthesizer",
+                        "Final Eclipse Gate",
+                        "Crystal Altar",
+                        "Champion Infuser"
+                    },
+                    Catalyst = "Catalyst of the Supreme"
+                },
+                ["Catalyst of Light"] = {
+                    name = "Optic Origin",
+                    items = {
+                        "True Book of Knowledge",
+                        "Morning Star",
+                        "Neutron Star",
+                        "Catalyzed Star",
+                        "Ore Nova",
+                        "Void Star"
+                    },
+                    Catalyst = "Catalyst of Light"
+                },
+                ["Catalyst of Earth"] = {
+                    name = "Havium Mine",
+                    items = {
+                        "True Book of Knowledge",
+                        "Yuttrium Mine",
+                        "Symmetryte Mine",
+                        "Yuntonium Mine",
+                        "Solarium Mine",
+                        "Gargantium Mine"
+                    },
+                    Catalyst = "Catalyst of Earth"
+                },
+                ["Catalyst of Death"] = {
+                    name = "The Death Cap",
+                    items = {
+                        "True Book of Knowledge",
+                        "Dreamer's Blight",
+                        "Deadly Spore",
+                        "Azure Spore",
+                        "Delta Phantom",
+                        "Lord of Tenebrous"
+                    },
+                    Catalyst = "Catalyst of Death"
+                },
+                ["Catalyst of Fire"] = {
+                    name = "Vulcan's Wrath",
+                    items = {
+                        "True Book of Knowledge",
+                        "Igneous Forge",
+                        "Searing Heat",
+                        "Spirit of Fire",
+                        "Firecrystallized System",
+                        "Vulcan's Destiny"
+                    },
+                    Catalyst = "Catalyst of Fire"
+                },
+                ["Catalyst of Fortune"] = {
+                    name = "Midas Blaster",
+                    items = {
+                        "True Book of Knowledge",
+                        "King Gold Mine",
+                        "Massive Diamond Drill",
+                        "Sage Justice",
+                        "Coliseum Catharsis",
+                        "Sage King"
+                    },
+                    Catalyst = "Catalyst of Fortune"
+                },
+                ["Catalyst of Power"] = {
+                    name = "Tyrant's Throne",
+                    items = {
+                        "True Book of Knowledge",
+                        "V-tolite Mine",
+                        "Tyrant's Forge",
+                        "Mystical Thunder",
+                        "Void Drive",
+                        "Gargantium Core"
+                    },
+                    Catalyst = "Catalyst of Power"
+                },
+                ["Catalyst of Nature"] = {
+                    name = "Garden of Gaia",
+                    items = {
+                        "True Book of Knowledge",
+                        "Gaia's Grasp",
+                        "Dreamer's Life",
+                        "Frozen Peaks",
+                        "Ambrosia Garden",
+                        "Deadly Spore"
+                    },
+                    Catalyst = "Catalyst of Nature"
+                },
+                ["Catalyst of Space"] = {
+                    name = "Ore Hypernova",
+                    items = {
+                        "True Book of Knowledge",
+                        "Optic Origin",
+                        "Blue Supergiant",
+                        "Aurora Borealis",
+                        "Stardust Pulsar",
+                        "Stellarite Mine"
+                    },
+                    Catalyst = "Catalyst of Space"
+                },
+                ["Catalyst of Knowledge"] = {
+                    name = "Enchanted Library",
+                    items = {
+                        "True Book of Knowledge",
+                        "Book of Knowledge",
+                        "Stardust Illuminator",
+                        "Statue of Knowledge",
+                        "Ore Indoctrinator",
+                        "Ancient Coliseum"
+                    },
+                    Catalyst = "Catalyst of Knowledge"
+                },
+                ["Catalyst of Time"] = {
+                    name = "The Hourglass",
+                    items = {
+                        "True Book of Knowledge",
+                        "Grandfather Clockwork",
+                        "Interstellar Conqueror",
+                        "Temporal Enchantment",
+                        "Temporal Armageddon",
+                        "The Trinity"
+                    },
+                    Catalyst = "Catalyst of Time"
+                },
+                ["Catalyst of Blood Magic"] = {
+                    name = "Swag City",
+                    items = {
+                        "True Book of Knowledge",
+                        "Draconicglass Mine",
+                        "Azure Purifier",
+                        "Dreamer's Nightmare",
+                        "Funky Town",
+                        "Devil's Spore"
+                    },
+                    Catalyst = "Catalyst of Blood Magic"
+                },
+                ["Catalyst of Necromancy"] = {
+                    name = "Methuselah's Mask",
+                    items = {
+                        "True Book of Knowledge",
+                        "The Death Cap",
+                        "Lord of Tenebrous",
+                        "Castle Bravo",
+                        "Tsar Bomba",
+                        "Son of Poison"
+                    },
+                    Catalyst = "Catalyst of Necromancy"
+                },
+                ["Catalyst of Magic"] = {
+                    name = "Meralin's Sorcery",
+                    items = {
+                        "True Book of Knowledge",
+                        "Forbidden Magic",
+                        "Mystical Thunder",
+                        "Swag City",
+                        "Methuselah's Mask",
+                        "Soul Blossom"
+                    },
+                    Catalyst = "Catalyst of Magic"
+                },
+                ["Catalyst of Oblivion"] = {
+                    name = "The Heart of Void",
+                    items = {
+                        "True Book of Knowledge",
+                        "Daestrophe",
+                        "Void Star",
+                        "Devourer of Nightmares",
+                        "Oblivion Emission",
+                        "The Forbidden Tome"
+                    },
+                    Catalyst = "Catalyst of Oblivion"
+                }
+            }
